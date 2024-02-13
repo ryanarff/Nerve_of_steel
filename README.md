e932cea5ecb4894fb2be51324ea6af8f55f7015c
ryanarff <159319182+ryanarff@users.noreply.github.com>
2024-02-13 18:14:49 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-# Timer
-### a MGTC28 Python example
-This is a timer program written in Python  
+# Nerve of Steel
+### an MGTC28 Python script for playing Nerve of Steel
+This Nerve of Steel is a party game where players sit in a circle.  When the game begins:
+
+1. The program displays "Players stand"
+
+2. The program sleeps for a random time between 5 to 25 seconds.  While the program is sleeping, players can sit down.  Keep track of the last person to sit down.
+
+3. When sleep is over, the program displays "Time Up.  Last to sit down wins."  Players still standing are eliminated, and the winner is the last person to sit down.
 
 ![times-up!](https://media.makeameme.org/created/times-up-5923e0.jpg)\
-timer.py is a simple Python script allowing users to set timer duration.
-Upon timer expiry, the user will see the time-up meme.
-timer.py uses the **time library** to help keep track of time and the **pillow library** to facilitate the meme display.
+timer.py uses the **time library** to help keep track of time and the **pillow library** to facilitate the image display.
+
+Cloned from Bill Chau's timer.py, github: lazigerbill
 
 Please see the documentation for the library used:
 - [time](https://docs.python.org/3/library/time.html)
 - [pillow](https://pypi.org/project/Pillow/)
+# Nerve-of-steel
+# Nerve-of-steel
--- timer.py
+++ timer.py
@@ -1,23 +1,30 @@
 """
-This program is used as an example for MGTC28.
-timer.py is a simple Python script that will allow user to set timer duration.
-Upon timer expiry, user will see the time up meme and sound notification.
-timer.py uses the time library to help keep track of time
+This program is an assignment for MGTC28.
+timer.py is a simple Python script for playing Nerve of Steel.
+Nerve of Steel is a party game where players sit in a circle.  When the game begins:
+
+1. The program displays "Players stand"
+
+2. The program sleeps for a random time between 5 to 25 seconds.  While the program is sleeping, players can sit down.  Keep track of the last person to sit down.
+
+3. When sleep is over, the program displays "Time Up.  Last to sit down wins."  Players still standing are eliminated, and the winner is the last person to sit down.
 """
 
 
-# This program is timer that counts down
+# This program is for Nerve of Steel
 
 
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
-from PIL import Image # the pillow library makes it easy to display images 
-
+from PIL import Image # the pillow library makes it easy to display images
+import random
 im = Image.open("times-up.jpeg")
 
-# ask user to enter desired countdown time
-set_time = int(input("Please set your timer in seconds: "))
+# displays "Players stand"
+print("Players stand")
 
-time.sleep(set_time)
+# sleeps for a random time between 5 to 25 seconds
+time.sleep(random.randint(5, 25))
 
-im.show()
+# program displays "Time Up.  Last to sit down wins."
+print("Time up. Last to sit down wins.")
 
